dcc6dde07b03b04d68b6fe24f25b3f435213966f
lewismati <lewis.mati@student.moringaschool.com>
2022-09-01 10:11:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-describe('index.js', function() {
-  describe('multiply', function() {
-    it("is an equation whose multiplied results will equal 62", function() {
-      expect(num1).to.be.a('number')
-      expect(num2).to.be.a('number')
-      expect(multiply).to.eq(62)
-    });
-  });
-  describe('random', function() {
-    it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
-    });
-  });
-  describe('mod', function() {
-    it("is an equation that calculates a remainder that is equal to 4", function() {
-      expect(num3).to.be.a('number')
-      expect(num4).to.be.a('number')
-      expect(mod).to.eq(4)
-    });
-  });
-  describe('max', function() {
-    it("will return 20 as the highest number in the set", function() {
-      expect(max).to.eq(20)
-    });
-  });
-});
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+
+const max = Math.max(5, 20, 10, 4);
